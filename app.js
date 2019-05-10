b516cc46c93b3ecacd67dd1d758ea204f1f9844a
Sean Morrison <sean1972@gmail.com>
2019-05-09 17:28:27 -0700

    Created a small database with a user prompt to find out info about which computers a user uses

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-let data = [1, 2, 3, 4, 5];
+let data = [
+  {
+    type: 'laptop',
+    user: 'John Smith',
+    make: 'Lenovo',
+    model: 'T460s',
+    os: 'Windows 10',
+    ver: 1809
+  },
+  {
+    type: 'desktop',
+    user: 'Jane Doe',
+    make: 'Dell',
+    model: 'Optiplex 390',
+    os: 'Windows 7',
+    ver: '6.1.7601'
+  }
+];
+
+let arrNum = prompt('Which user would you like to know about?', '');
+
+console.log(
+  `${data[arrNum].user} uses a ${data[arrNum].make} ${data[arrNum].model} ${
+    data[arrNum].type
+  } with ${data[arrNum].os} version ${data[arrNum].ver}.`
+);
