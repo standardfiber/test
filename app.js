fecac17cb5ca62568eebe3540cbe2e56e9266edb
Sean Morrison <sean1972@gmail.com>
2019-05-14 15:40:41 -0700

    JavaScript closure example

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
-let data = [
-  {
-    type: 'laptop',
-    user: 'John Smith',
-    make: 'Lenovo',
-    model: 'T460s',
-    os: 'Windows 10',
-    ver: 1809
-  },
-  {
-    type: 'desktop',
-    user: 'Jane Doe',
-    make: 'Dell',
-    model: 'Optiplex 390',
-    os: 'Windows 7',
-    ver: '6.1.7601'
-  },
-  {
-    type: 'laptop',
-    user: 'Tim Cook',
-    make: 'Apple',
-    model: 'MacBook Pro',
-    os: 'macOS',
-    ver: 'Mojave'
+function a() {
+  var x = 'x';
+
+  function b() {
+    var y = 'y';
+    return x + y;
   }
-];
+  return b();
+}
 
-let arrNum = prompt('Which user would you like to know about?', '');
-
-console.log(
-  `${data[arrNum].user} uses a ${data[arrNum].make} ${data[arrNum].model} ${
-    data[arrNum].type
-  } with ${data[arrNum].os} version ${data[arrNum].ver}.`
-);
+console.log(a());
