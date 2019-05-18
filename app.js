7b342fb890e25a2874cade7238a206e0e2d660af
Sean Morrison <seanmorrison@standardfiber.com>
2019-05-17 17:38:57 -0700

    Created function that appends number to end of array

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-// function nextInLine(arr, item) {
-//   return item;
-// }
+/*
+Instructions: Push a number to the end of an array.
+1. Create array with numbers.
+2. Create a function that takes a parameter arr and returns it.
+3. Print the function to the console.
+4. Add an item parameter to the function.
+5. Push the item to the array.
+6. Notice how the item is undefined.
+7. Add a number argument to be pushed.
+8. Stringify the results.
+*/
 
-function nextInLine(arr, item) {
-  // Your code here
+let arr = [1, 2, 3, 4, 5];
+
+function nextInArr(arr, item) {
   arr.push(item);
-  return arr.shift(); // Change this line
+  return arr;
 }
 
-var testArr = [1, 2, 3, 4, 5];
+let results = JSON.stringify(nextInArr(arr, 6));
 
-console.log('Before: ' + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log('After: ' + JSON.stringify(testArr));
+console.log(results);
